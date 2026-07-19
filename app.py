5be72c4eea64f4b58228c239327f704fe6287881
twinkelglue <dda26-2610508@gclass.ice.go.kr>
2026-07-19 22:10:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -738,3 +738,6 @@ def open_chat_ban_user(room_id):
     cur.close()
     conn.close()
     return redirect(url_for('open_chat_room', room_id=room_id))
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
